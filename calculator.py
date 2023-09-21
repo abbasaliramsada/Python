548e24aa3580c0e76958846c458d0b7184ba9f67
abbasaliramsada <abbasaliramsada13@gmail.com>
2023-09-21 22:28:18 +0530

    Simple Calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+first = input("Enter first value : ")
+second = input("Enter second value : ")
+
+add = int(first) + int(second)
+sub = int(first) - int(second)
+multi = int(first) * int(second)
+div = float(first) / float(second)
+
+print(add)
+print(sub)
+print(multi)
+print(div)
